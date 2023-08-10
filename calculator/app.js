e85cc07f2d5389a366c4258acaa256a2a8b60dcc
Nicole Shafer <nicolershafer@gmail.com>
2023-08-10 12:35:40 -0400

    JS

--- calculator/app.js
+++ calculator/app.js
@@ -1,7 +1,25 @@
 const buttons = document.querySelectorAll("button")
-const screenDisplay = document.querySelectorAll('.span')
+const screenDisplay = document.querySelector('.span')
 
-const calculate = () =>{
+let calculation = []
+let accumulativeCalculation;
 
+const calculate = (button) =>{
+    const value = button.textContent
+    if(value === "AC"){
+        calculation = []
+        screenDisplay.textContent = " 0 "
+    }   else if(value === "=" ){
+        screenDisplay.textContent  = eval(accumulativeCalculation)
+    }  
+     else{
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
+
+    console.log(calculation)
 }
+
 buttons.forEach(button => button.addEventListener("click", () => calculate(button) ))
+
