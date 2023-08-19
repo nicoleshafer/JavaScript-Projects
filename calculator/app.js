019d57982ed245830bce9cf8021e1d71ba02ecc7
Nicole Shafer <nicolershafer@gmail.com>
2023-08-19 11:55:57 -0400

    8/19

--- calculator/app.js
+++ calculator/app.js
@@ -3,16 +3,28 @@ const screenDisplay = document.querySelector('.span')
 
 let calculation = []
 let accumulativeCalculation;
-
+let positiveNumber = true
 const calculate = (button) =>{
     const value = button.textContent
     if(value === "AC"){
         calculation = []
         screenDisplay.textContent = " 0 "
-    }   else if(value === "=" ){
+    }  else if(value === "=" ){
         screenDisplay.textContent  = eval(accumulativeCalculation)
-    }  
-     else{
+    }  else if(value === "%"){
+        if(calculation.length > 0){
+            const percentValue = parseFloat(calculation.join("")) / 100
+            accumulativeCalculation = [percentValue.toString()]
+            accumulativeCalculation = percentValue.toString()
+            screenDisplay.textContent = accumulativeCalculation;
+            
+        }} else if(value ==="+/-"){
+        positiveNumber = !positiveNumber
+        if(!positiveNumber){
+            calculation = ["-" + calculation.join("")]
+            accumulativeCalculation = calculation.join("")
+        }
+    } else{
         calculation.push(value)
         accumulativeCalculation = calculation.join('')
         screenDisplay.textContent = accumulativeCalculation
